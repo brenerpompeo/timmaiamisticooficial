52fc61f61831686cae60d7c22ffbcedfbfa97d87
brenerpompeo <158225596+brenerpompeo@users.noreply.github.com>
2025-09-03 22:33:05 -0300

    v10

---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./assets/**/*.{js,html}"],
+  darkMode: "class",
+  theme: { extend: {} },
+  plugins: [],
+};
